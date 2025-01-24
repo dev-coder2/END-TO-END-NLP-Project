36f8348aef78c56f7159935a79fe2375e82cf7f3
Dev Kumar Acharya <devacharya248@gmail.com>
2025-01-24 20:38:25 +0530

    model trainer and eval. pipeline added

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
 from src.textsummarizer.pipeline.data_ingestion_pipeline import DataIngestionPipeline
 from src.textsummarizer.pipeline.data_transformation_pipeline import DataTransformationPipeline
+from src.textsummarizer.pipeline.model_trainer_pipeline import ModelTrainerTrainingPipeline
+from src.textsummarizer.pipeline.model_evaluation_pipeline import ModelEvaluationTrainingPipeline
 from src.textsummarizer.logging import logger 
 
 STAGE_NAME="Data Ingestion"
@@ -25,3 +27,25 @@ try:
 except Exception as e:
     logger.exception(e)
     raise e 
+
+STAGE_NAME="Model Trainer stage"
+
+try:
+    logger.info(f"stage {STAGE_NAME} initiated")
+    model_trainer_pipeline=ModelTrainerTrainingPipeline()
+    model_trainer_pipeline.initiate_model_trainer()
+    logger.info(f"Stage {STAGE_NAME} Completed")
+except Exception as e:
+    logger.exception(e)
+    raise e
+
+STAGE_NAME = "Model Evaluation stage"
+try: 
+   logger.info(f"*******************")
+   logger.info(f">>>>>> stage {STAGE_NAME} started <<<<<<")
+   model_evaluation = ModelEvaluationTrainingPipeline()
+   model_evaluation.initiate_model_evaluation()
+   logger.info(f">>>>>> stage {STAGE_NAME} completed <<<<<<\n\nx==========x")
+except Exception as e:
+        logger.exception(e)
+        raise e
